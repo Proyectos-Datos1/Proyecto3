f062a4211a25b31b0c6228dad4192f70753727fd
geraldvm <gerald-_16@hotmail.com>
2017-11-15 22:24:35 -0600

    RecyclerView

--- RecyclerView/app/src/main/java/org/tec/datos/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/org/tec/datos/recyclerview/MainActivity.java
@@ -2,12 +2,36 @@ package org.tec.datos.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private List<String> names;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mlayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.names= this.getAllNames();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+    }
+    private List<String> getAllNames(){
+        return new ArrayList<String>(){{
+            add("Carlos");
+            add("Fiorella");
+            add("Daniel");
+            add("Jose");
+            add("Juan");
+            add("Pamela");
+
+
+        }};
     }
 }
--- RecyclerView/app/src/main/java/org/tec/datos/recyclerview/MyAdapter.java
+++ RecyclerView/app/src/main/java/org/tec/datos/recyclerview/MyAdapter.java
@@ -1,4 +1,5 @@
 package org.tec.datos.recyclerview;
+
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -6,39 +7,65 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.List;
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
 
     private List<String> names;
     private int layout;
-    private OnItemClickListener listener;
+    private OnItemClickListener itemClickListener;
+
+    public MyAdapter(List<String> names,int layout,OnItemClickListener listener){
+        this.names=names;
+        this.layout=layout;
+        this.itemClickListener=listener;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(layout,parent,false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.bind(names.get(position),itemClickListener);
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return names.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        // Elementos UI a rellenar
-        public TextView name;
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView textViewName;
 
         public ViewHolder(View itemView) {
-            // Recibe la View completa. La pasa al constructor padre y enlazamos referencias UI
-            // con nuestras propiedades ViewHolder declaradas justo arriba.
             super(itemView);
-            this.name = (TextView) itemView.findViewById(R.id.textViewName);
+            this.textViewName = (TextView) itemView.findViewById(R.id.textViewName);
+        }
+
+        /**
+         *
+         * @param name Se le pasa el tipo de elemento (puede ser frutas
+         * @param listener
+         */
+        public void bind(final String name, final OnItemClickListener listener){
+            this.textViewName.setText(name);
+            //fruta.getname()
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.onItemClick(name,getAdapterPosition());
+                }
+            });
         }
     }
-    // Declaramos nuestra interfaz con el/los método/s a implementar
-    public interface OnItemClickListener {
+
+    public interface OnItemClickListener{
         void onItemClick(String name, int position);
     }
 }
